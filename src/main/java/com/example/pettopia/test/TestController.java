9f0927a3049e0907b26f723f53f57ade81a1d525
jeooooooong2 <jeong27040@gmail.com>
2024-12-26 15:12:22 +0900

    우정 : 템플릿 추가

---
+++ src/main/java/com/example/pettopia/test/TestController.java
@@ -0,0 +1,16 @@
+package com.example.pettopia.test;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+public class TestController {
+	@GetMapping("/test")
+	public String getMethodName() {
+		log.debug("index");
+		return "index";
+	}
+	
+}
