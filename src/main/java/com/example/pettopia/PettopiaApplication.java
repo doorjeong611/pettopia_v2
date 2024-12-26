44c8b31d759d21d5d07c8f4c5adc3439c184e1c7
KDH <kimdongh7277@naver.com>
2024-12-26 11:11:33 +0900

    Initial commit

---
+++ src/main/java/com/example/pettopia/PettopiaApplication.java
@@ -0,0 +1,13 @@
+package com.example.pettopia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PettopiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PettopiaApplication.class, args);
+	}
+
+}
