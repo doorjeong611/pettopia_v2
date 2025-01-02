6ff7c56a3818dbb69fc0b3e0f55cbe0e0a1e1917
KDH <kimdongh7277@naver.com>
2025-01-02 10:02:21 +0900

    동현 : template 수정

--- src/main/java/com/example/pettopia/test/TestController.java
+++ src/main/java/com/example/pettopia/test/TestController.java
@@ -3,7 +3,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
