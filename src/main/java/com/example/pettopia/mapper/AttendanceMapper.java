72552e8080b4b8bb1041aa1057e1a6d7343b9251
jayoon-oh <dhwkdbs3@gmail.com>
2025-01-05 15:47:40 +0900

    오자윤 : AttendanceController if문 추가

--- src/main/java/com/example/pettopia/mapper/AttendanceMapper.java
+++ src/main/java/com/example/pettopia/mapper/AttendanceMapper.java
@@ -6,6 +6,9 @@ import com.example.pettopia.vo.Attendance;
 
 @Mapper
 public interface AttendanceMapper {
+	// 오자윤 : /employee/attendanceOn & Off 근태기록 조회
+	Integer getAttendanceExists(Attendance attendance);
+	
 	// 오자윤 : /employee/attendanceOn 근태 : 출근
 	Integer insertAttendanceOn(Attendance attendance);
 	
