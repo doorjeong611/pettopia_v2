1da1b7263d7889479d0e7abf94d968f638193509
KDH <kimdongh7277@naver.com>
2025-01-06 15:05:13 +0900

    동현 : TeamColor Util 추가

--- src/main/java/com/example/pettopia/controller/MainController.java
+++ src/main/java/com/example/pettopia/controller/MainController.java
@@ -3,13 +3,23 @@ package com.example.pettopia.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.pettopia.util.TeamColor;
+
+import lombok.extern.slf4j.Slf4j;
+
 
 @Controller
+@Slf4j
 public class MainController {
 	
 	// 메인페이지
 	@GetMapping("/common/main")
 	public String mainPage() {
+		log.debug(TeamColor.KDH + "Test Log Color" + TeamColor.RESET);
+		log.debug(TeamColor.KMJ + "Test Log Color" + TeamColor.RESET);
+		log.debug(TeamColor.OJY + "Test Log Color" + TeamColor.RESET);
+		log.debug(TeamColor.WJ + "Test Log Color" + TeamColor.RESET);
+		log.debug(TeamColor.LJH + "Test Log Color" + TeamColor.RESET);
 		return "common/main";
 	}
 }
---
+++ src/main/java/com/example/pettopia/util/TeamColor.java
@@ -0,0 +1,12 @@
+package com.example.pettopia.util;
+
+public class TeamColor {
+	
+	public static String KDH = "\u001B[34m"; // 김동현(파란색)
+	public static String KMJ = "\u001B[32m"; // 김문정(초록색)
+	public static String OJY = "\u001B[38;5;214m"; // 오자윤(주황색)
+	public static String WJ = "\u001B[38;5;135m"; // 우정(보라색)
+	public static String LJH = "\u001B[33m"; // 이준호(노란색)
+	public static String RESET = "\u001B[0m"; // RESET
+
+}
