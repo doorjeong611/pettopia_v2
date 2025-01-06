960fbbebad5cdb869e5d9705f33ac33a84001712
KDH <kimdongh7277@naver.com>
2025-01-06 17:30:00 +0900

    동현 : DocumentController 생성

---
+++ src/main/java/com/example/pettopia/service/DocumentService.java
@@ -0,0 +1,13 @@
+package com.example.pettopia.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Transactional
+@Slf4j
+public class DocumentService {
+
+}
