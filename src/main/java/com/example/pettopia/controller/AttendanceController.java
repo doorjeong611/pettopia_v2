979d6c8ac2e17757f91f4c3e6cac33f0a35f1bf1
jayoon-oh <dhwkdbs3@gmail.com>
2025-01-06 12:29:16 +0900

    오자윤 : 수정

--- src/main/java/com/example/pettopia/controller/AttendanceController.java
+++ src/main/java/com/example/pettopia/controller/AttendanceController.java
@@ -33,25 +33,25 @@ public class AttendanceController {
 	    LocalTime sixPmEnd = LocalTime.of(18, 0);
 	    
 	    // 출근 상태 가져오기
-	    String attendanceStatus = attendance.getAttendance_status();
+	    String attendanceStatus = attendance.getAttendanceDate();
 	    
 	    if ("연차".equals(attendanceStatus)) {
-	        attendance.setAttendance_status("연차");
+	        attendance.setAttendanceDate("연차");
 	    } else if ("반차".equals(attendanceStatus)) {
-	        attendance.setAttendance_status("반차");
+	        attendance.setAttendanceDate("반차");
 	    } else if (attendanceService.getAttendanceStatus(attendance) > 0) {
 	        // 출근 기록이 있는 경우
 	        if (startTime.isBefore(nineAmStart)) {
-	            attendance.setAttendance_status("출근");
+	            attendance.setAttendanceDate("출근");
 	        // 9시 이후 출근 & 6시 퇴근 기록 있는 경우
 	        } else if (startTime.isAfter(nineAmStart) && startTime.isBefore(sixPmEnd)) { 
-	            attendance.setAttendance_status("지각");
+	            attendance.setAttendanceDate("지각");
 	        } else {
-	            attendance.setAttendance_status("조퇴");
+	            attendance.setAttendanceDate("조퇴");
 	        }	
 	    // 출근 기록이 없는 경우	        
 	    } else {
-	        attendance.setAttendance_status("결근");
+	        attendance.setAttendanceDate("결근");
 	    }
 	    
 	    // 출근등록 : insertAttendanceOn 쿼리실행
@@ -69,7 +69,7 @@ public class AttendanceController {
         LocalTime endTime = LocalTime.parse(endTimeStr);
         
         // 퇴근 상태 설정 
-        attendance.setAttendance_status("퇴근");
+        attendance.setAttendanceDate("퇴근");
         // 퇴근등록 : insertAttendanceOff 쿼리실행
         attendanceService.attendanceOff(attendance);
         log.debug(attendance.toString() + "-----> attendance");
