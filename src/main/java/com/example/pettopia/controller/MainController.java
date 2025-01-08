d59433abb7d4a2ff4b67e6905b9c5f475d362287
jayoon-oh <dhwkdbs3@gmail.com>
2025-01-08 14:56:05 +0900

    오자윤 : 메인(근태) 구현완료

--- src/main/java/com/example/pettopia/controller/MainController.java
+++ src/main/java/com/example/pettopia/controller/MainController.java
@@ -1,5 +1,7 @@
 package com.example.pettopia.controller;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +10,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.pettopia.service.AttendanceSerivce;
+import com.example.pettopia.util.TeamColor;
 import com.example.pettopia.vo.Attendance;
+import com.example.pettopia.vo.Employee;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 
 
@@ -20,8 +25,21 @@ public class MainController {
 	
 	// 오자윤 : 출퇴근 기록 조회
 	@GetMapping("/common/petTopiaMain")
-	public String mainPage(Model model, Attendance attendnace) {
-		List<Attendance> attendanceList = attendanceService.getAttendance(attendnace);
+	public String mainPage(Model model, Attendance attendance, HttpSession session) {
+		//세션 가져오기.
+		Employee loginEmp = (Employee) session.getAttribute("loginEmp");
+		String empNo = loginEmp.getEmpNo();
+		attendance.setEmpNo(empNo);
+		
+		// 현재 날짜 설정
+	    String currentDate = LocalDate.now().toString(); 
+	    attendance.setAttendanceDate(currentDate); // 출근 날짜 설정
+		
+		// 출근날짜 currentDate 설정.
+		attendance.setAttendanceDate(currentDate); 
+		    
+		List<Attendance> attendanceList = attendanceService.getAttendance(attendance);
+		log.debug(TeamColor.OJY + "attendanceList------> " + attendanceList + TeamColor.RESET);
 		
 		model.addAttribute("attendanceList", attendanceList);
 		return "common/petTopiaMain";
