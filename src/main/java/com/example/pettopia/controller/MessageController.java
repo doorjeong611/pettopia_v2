865f53fd5c27020c7f3fe624dbe30da6ec0becaa
jayoon-oh <dhwkdbs3@gmail.com>
2025-01-09 14:13:34 +0900

    오자윤 : 쪽지리스트 탬플릿 적용

---
+++ src/main/java/com/example/pettopia/controller/MessageController.java
@@ -0,0 +1,18 @@
+package com.example.pettopia.controller;
+
+import org.springframework.stereotype.Controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Slf4j
+@Controller
+public class MessageController {
+@GetMapping("message/messageList")
+public String getMethodName() {
+    return "message/messageList";
+}
+
+}
