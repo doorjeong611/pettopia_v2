68e1f63de42bfa99e0c5e34f7176d2c31bf40f30
jayoon-oh <dhwkdbs3@gmail.com>
2025-01-10 14:33:54 +0900

    오자윤 : 쪽지휴지통 기능 구현

--- src/main/java/com/example/pettopia/message/MessageController.java
+++ src/main/java/com/example/pettopia/message/MessageController.java
@@ -1,14 +1,20 @@
 package com.example.pettopia.message;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.pettopia.util.TeamColor;
 import com.example.pettopia.vo.Employee;
@@ -20,6 +26,29 @@ import jakarta.servlet.http.HttpSession;
 @Controller
 public class MessageController {
 	@Autowired MessageService messageService;
+	
+	
+@PostMapping("/message/messageBin")
+public String messageBin(HttpSession session, @RequestParam(value="messageNo",required=false) ArrayList<String> messageNo) {
+	// Employee loginEmp = (Employee) session.getAttribute("loginEmp");
+	
+	log.debug(TeamColor.OJY + "messageNo------>" + messageNo + TeamColor.RESET);
+		messageService.moveToBin(messageNo);
+	
+	return "redirect:/message/messageList";
+}
+
+	
+@PostMapping("/message/readMessage")
+public String readMessage(HttpSession session, String messageNo) {
+	Employee loginEmp = (Employee) session.getAttribute("loginEmp");
+	String empNo = loginEmp.getEmpNo();
+
+    // 읽음 상태로 변경
+    messageService.markAsRead(messageNo);
+    return "redirect:/message/messageList";
+}
+
 	
 @GetMapping("message/messageList")
 public String getMethodName(HttpSession session, Model model) {
--- src/main/java/com/example/pettopia/message/MessageMapper.java
+++ src/main/java/com/example/pettopia/message/MessageMapper.java
@@ -5,11 +5,20 @@ import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.example.pettopia.vo.Attendance;
+import com.example.pettopia.vo.Message;
+
 
 @Mapper
 public interface MessageMapper {
 	
 	// 오자윤 : /employee/messageList 쪽지리스트
 	List<Map<String, Object>>getMessageList(String empNo);
+	
+	// 오자윤 : /employee/messageList 쪽지 휴지통 이동 -->
+	Integer updateMessage(String messageNo);
+	
+	// 오자윤 : /employee/messageList 메시지 읽음 상태 변환 -->
+	Integer updateMessageState(String message);
 	
 }
