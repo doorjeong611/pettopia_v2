4d90700cf801560b0a1165d6118194d07d7b51bf
munjeong <moonj654@naver.com>
2025-01-13 15:25:36 +0900

    문정 : 시큐리티 수정

--- src/main/java/com/example/pettopia/common/EmpUserDetailsService.java
+++ src/main/java/com/example/pettopia/common/EmpUserDetailsService.java
@@ -9,42 +9,23 @@ import org.springframework.stereotype.Service;
 
 import com.example.pettopia.dto.EmpUserDetails;
 import com.example.pettopia.employee.EmployeeMapper;
-import com.example.pettopia.util.TeamColor;
 import com.example.pettopia.vo.Employee;
 
 import lombok.extern.slf4j.Slf4j;
 
-@Service 
 @Slf4j
-// 로그인 검증을 위해 db에서 정보 조회
-public class EmpUserDetailsService implements UserDetailsService{
+@Service
+public class EmpUserDetailsService implements UserDetailsService {
 
-	// DB에서 가져온 user 정보 검증
-	// DB에 접근하기 위해 연결해줘야 함. JPA == repository, mybatis == mapper 사용
-	@Autowired private EmployeeMapper employeeMapper;
-	
-	
-	
-	
+	@Autowired EmployeeMapper employeeMapper;
+
 	@Override
 	public UserDetails loadUserByUsername(String empNo) throws UsernameNotFoundException {
-		
-		log.debug(TeamColor.KMJ +"EmpUserDetailsService : 로그인 진행 중");
-		log.debug(TeamColor.KMJ +"EmpUserDetailsService empNo" + empNo);
-		
-		Employee emp = employeeMapper.selectEmployeeInfo(empNo);
-		if(emp != null) {
-			log.debug(TeamColor.KMJ +"성공" );
-			log.debug(TeamColor.KMJ +"emp.toString() : " + emp.toString() );
-			return new EmpUserDetails(emp); // db 조회시 정보가 있다면 반환하여 로그인 진행 . EmpUserDetails클래스 생성자를 이용하여 직원 정보 넣어서 반환
-		}
+		Employee employee = employeeMapper.selectEmployeeInfo(empNo);
+		EmpUserDetails details = new EmpUserDetails(employee);
+		log.debug("details:--------------"+details.toString());
 		
 		
-		
-		return null; // 로그인 실패
+		return details;
 	}
-
-
-	
-	
 }
