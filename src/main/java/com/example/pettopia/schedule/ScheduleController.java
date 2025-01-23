f293554a41f60319b78d9044f2e4196fe3211216
KDH <kimdongh7277@naver.com>
2025-01-23 19:04:41 +0900

    동현 : Schedule List 조회

--- src/main/java/com/example/pettopia/schedule/ScheduleController.java
+++ src/main/java/com/example/pettopia/schedule/ScheduleController.java
@@ -1,19 +1,26 @@
 package com.example.pettopia.schedule;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 @Slf4j
 public class ScheduleController {
 	
-	@GetMapping("/schedule/schedule")
-	public String getMethodName(@RequestParam String param) {
-		return new String();
+	// scheduleCalendar Form
+	@GetMapping("/schedule/scheduleCalendar")
+	public String scheduleCalendar(Model model) {
+		return "schedule/scheduleCalendar";
+	}
+	
+	//scheduleList Form
+	@GetMapping("/schedule/scheduleList")
+	public String scheduleList(Model model) {
+		return "schedule/scheduleList";
 	}
 	
 
