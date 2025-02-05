4d3e866549e1af33307706701b61b71f886e3893
doorjeong611 <doorjeong611@gmail.com>
2025-02-05 12:48:06 +0900

    문정 : 로그인 맵핑 수정

--- src/main/java/com/example/pettopia/common/LoginController.java
+++ src/main/java/com/example/pettopia/common/LoginController.java
@@ -24,7 +24,7 @@ public class LoginController {
 	@Autowired EmployeeService employeeService;
 	
 	// 로그인 폼으로 이동
-	@GetMapping("/login")
+	@GetMapping({"/","/login"})
 	public String login(Authentication authentication) {
 		
 		if (authentication != null && authentication.isAuthenticated()) {
