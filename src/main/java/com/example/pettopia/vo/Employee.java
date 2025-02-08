b2e147a04ffa2ea74235053b12cc5d1a727bf130
khyun27 <kimdongh727@gmail.com>
2025-02-08 20:14:10 +0900

    동현 : Serializable 오류 수정

--- src/main/java/com/example/pettopia/vo/Employee.java
+++ src/main/java/com/example/pettopia/vo/Employee.java
@@ -1,10 +1,13 @@
 package com.example.pettopia.vo;
 
+import java.io.Serializable;
+
 import lombok.Data;
 
 @Data
-public class Employee {
+public class Employee implements Serializable {
 	
+	private static final long serialVersionUID = 1L;
 	private String empNo; // PK
 	private String empName;
 	private String empPw;
